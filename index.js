87abdac0427356e8672f6b7faa6c4cb80d6a2021
Natan PC <natanr123@gmail.com>
2018-08-14 17:23:17 +0300

    adding shuffle

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 function randomIntRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-module.exports = {randomIntRange};
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+module.exports = { randomIntRange, shuffle };
